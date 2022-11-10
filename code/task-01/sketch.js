aea6e82c303755eaccabf52bb3a50bc288339e75
henni4g <117078111+henni4g@users.noreply.github.com>
2022-11-10 23:17:37 +0100

    task-02

--- code/task-01/sketch.js
+++ code/task-01/sketch.js
@@ -11,28 +11,37 @@ function draw() {
   background("#7aaeff");
   noStroke();
   
+  //Regenbogen:
+
+  //pink
   fill("#f2308e")
   circle(200, 350, 500)
 
+  //Orange
   fill("#f7861b")
   circle(200, 375, 500)
 
+  //Gelb
   fill("#ffee00")
   circle(200, 400, 500)
 
+  //Grün
   fill("#9dff00")
   circle(200, 425, 500)
 
+  //Blau
   fill("#00e1ff")
   circle(200, 450, 500)
 
+  //Lila
   fill("#b127e3")
   circle(200, 475, 500)
 
+  //Hintergrundfarbe zum cutten
   fill("#7aaeff")
   circle(200, 500, 500)
 
-
+//Wolken:
   fill(240)
   circle(0,380, 100)
   circle(50,400, 100)
@@ -42,6 +51,7 @@ function draw() {
   circle(350,400, 100)
   circle(300,420, 100)
 
+  //Herz:
   fill("red")
   quad(202, 199, 175, 225, 200, 250, 225, 225);
 	arc(188, 213, 35, 35, PI - QUARTER_PI, 0);
---
+++ code/task-02/sketch.js
@@ -0,0 +1,50 @@
+function preload(){
+  // preload assets
+}
+
+let rectA = 50;
+let rectB = 50;
+let a = 20;
+let b = 20;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background (255)
+  blendMode(DARKEST)
+
+  line(0,93,200,93)
+  line(307,0,307,200)
+  line(400,307,200,307)
+  line(93,400,93,200)
+  
+
+  fill(255);
+  let x = 3
+  while (x >1 ) {
+    let y = 3 ;
+    while (y >1 ) {
+      square(200 - x*25 , 200 - x*25 , 50 * x);
+      
+      y -= 1;
+    }
+    x -= 1
+  }
+  rotate(Math.PI / 180 * 45)
+  fill(255);
+  let z = 3
+  while (z >1 ) {
+    let w = 3 ;
+    while (w >1 ) {
+      square(283 - z*25 ,0 - z*25 , 50 * z);
+      
+      w -= 1;
+    }
+    z -= 1
+  }
+
+   
+}
+
